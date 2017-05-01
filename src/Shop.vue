3279255ad1c6f7da074097858d59acefa0e4270f
Krishna Kumar <kks32@cam.ac.uk>
2017-05-01 19:58:07 +0100

    :ticket: Tickets

--- src/Shop.vue
+++ src/Shop.vue
@@ -9,8 +9,7 @@
       Purchase is cancelled!
       <v-btn flat class="pink--text">Close</v-btn>
     </v-snackbar>
-
-    <v-stepper v-model="purchasestep">
+    <v-stepper v-model="purchasestep" elevation-0>
       <v-stepper-header>
         <v-stepper-step step="1" v-bind:complete="purchasestep > 1">Select date and session</v-stepper-step>
         <v-divider />
@@ -22,19 +21,19 @@
         <Calendar></Calendar>
         <Session></Session>
         <v-btn primary @click.native="purchasestep = 2">Continue</v-btn>
-        <v-btn flat @click.native="cancelpurchase = true, purchasestep = 1">Cancel</v-btn>
+        <v-btn flat @click.native="cancelpurchase = true, purchasestep = 1" class="grey lighten-2"><v-icon left>remove_circle</v-icon>Cancel</v-btn>
       </v-stepper-content>
       <v-stepper-content step="2">
         <Tickets></Tickets>
         <v-btn primary @click.native="purchasestep = 3">Continue</v-btn>
-        <v-btn flat @click.native="purchasestep = 1">Back</v-btn>
-        <v-btn flat @click.native="cancelpurchase = true, purchasestep = 1">Cancel</v-btn>
+        <v-btn flat @click.native="purchasestep = 1" class="grey lighten-3"><v-icon left>arrow_back</v-icon>Back</v-btn>
+        <v-btn flat @click.native="cancelpurchase = true, purchasestep = 1" class="grey lighten-3"><v-icon left>remove_circle</v-icon>Cancel</v-btn>
       </v-stepper-content>
       <v-stepper-content step="3">
         <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px" />
         <v-btn primary @click.native="">Buy</v-btn>
-        <v-btn flat @click.native="purchasestep = 2">Back</v-btn>
-        <v-btn flat @click.native="cancelpurchase = true, purchasestep = 1">Cancel</v-btn>
+        <v-btn flat @click.native="purchasestep = 2" class="grey lighten-3"><v-icon left>arrow_back</v-icon>Back</v-btn>
+        <v-btn flat @click.native="cancelpurchase = true, purchasestep = 1" class="grey lighten-3"><v-icon left>remove_circle</v-icon>Cancel</v-btn>
       </v-stepper-content>
     </v-stepper>
 
--- src/components/Tickets.vue
+++ src/components/Tickets.vue
@@ -1,39 +1,62 @@
 <template>
-  <v-card class="grey lighten-4 elevation-0">
-    <v-card-text>
+<div id="tickets">
+  <v-card class="grey lighten-4 elevation-1">
+    <v-card-row class="purple darken-4">
+      <v-card-title>
+        <span class="white--text">Tickets</span>
+        <v-spacer></v-spacer>
+      </v-card-title>
+    </v-card-row>
+    <v-card-text class="grey lighten-4 elevation-1">
       <v-container fluid>
         <v-row>
           <v-col xs6>
-            <v-subheader v-text="'Adult tickets'" />
+            <v-subheader v-text="'Category'" />
+          </v-col>
+          <v-col xs3>
+            <v-subheader v-text="'# of tickets'" />
+          </v-col>
+          <v-col xs3>
+            <v-subheader v-text="'Total'" />
           </v-col>
           <v-col xs6>
-            <v-select
-              label="Select"
-              v-bind:items="items"
-              v-model="adult"
-              v-bind:rules="[() => adult && adult > 0 || 'Please select at least one ticket']"
-              light
-              item-value="number"
-            />
+            <v-subheader v-text="'Adult tickets'" />
+          </v-col>
+          <v-col xs3>
+            <v-select label="Select" v-bind:items="items" v-model="adult" v-bind:rules="[() => ((adult && adult > 0) || concession > 0) || 'Please select at least one ticket']"  max-height="200" light item-value="number" single-line auto />
+          </v-col>
+          <v-col xs3>
+            <p>{{ adultprice }}</p>
           </v-col>
           <v-col xs6>
             <v-subheader v-text="'Children'" />
           </v-col>
-          <v-col xs6>
-            <v-select
-              v-bind:items="items"
-              v-model="children"
-              label="Select"
-              class="input-group--focused"
-              light
-              item-value="number"
-            />
+          <v-col xs3>
+            <v-select v-bind:items="items" v-model="children" label="Select" max-height="200" light single-line auto />
           </v-col>
-
+          <v-col xs3>
+            <p>{{ childprice }}</p>
+          </v-col>
+          <v-col xs6>
+            <v-subheader v-text="'Concession'" />
+          </v-col>
+          <v-col xs3>
+            <v-select label="Select" v-bind:items="items" v-model="concession" v-bind:rules="[() => ((concession && concession > 0) || adult > 0) || 'Please select at least one ticket']" max-height="200" light item-value="number" single-line auto />
+          </v-col>
+          <v-col xs3>
+            <p>{{ concessionprice }}</p>
+          </v-col>
+          <v-col xs9>
+            <h4>Total</h4>
+          </v-col>
+          <v-col xs3>
+            <p>total</p>
+          </v-col>
         </v-row>
       </v-container>
     </v-card-text>
   </v-card>
+</div>
 </template>
 
 <script>
@@ -41,10 +64,28 @@ export default {
   'name': 'Tickets',
   data () {
     return {
-      adult: 1,
+      adult: 0,
       children: 0,
+      concession: 0,
       items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+  },
+  computed: {
+    adultprice: () => {
+      return this.adult * 5
+    },
+    childprice: () => {
+      return this.child
+    },
+    concessionprice: () => {
+      return this.concession
     }
   }
 }
 </script>
+
+<style scoped>
+.tickets {
+  z-index: 998
+}
+</style scoped>
