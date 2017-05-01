1f966682dc907caf24ce449cf64b8628a429efe8
Krishna Kumar <kks32@cam.ac.uk>
2017-05-01 12:52:04 +0100

    :computer: Stepper through purchase

--- src/Shop.vue
+++ src/Shop.vue
@@ -1,7 +1,43 @@
 <template>
   <div id="shop">
-    <Calendar></Calendar>
-    <Session></Session>
+    <!-- Cancel project notification -->
+    <v-snackbar
+      :timeout="3000"
+      :bottom="true"
+      v-model="cancelprj"
+    >
+      Project is cancelled!
+      <v-btn flat class="pink--text" @click.native="cancelprj = false">Close</v-btn>
+    </v-snackbar>
+
+    <v-stepper v-model="prj">
+      <v-stepper-header>
+        <v-stepper-step step="1" v-bind:complete="prj > 1">Select date and session</v-stepper-step>
+        <v-divider />
+        <v-stepper-step step="2" v-bind:complete="prj > 2">Select tickets</v-stepper-step>
+        <v-divider />
+        <v-stepper-step step="3">Review and pay</v-stepper-step>
+      </v-stepper-header>
+      <v-stepper-content step="1">
+        <Calendar></Calendar>
+        <Session></Session>
+        <v-btn primary @click.native="prj = 2">Continue</v-btn>
+        <v-btn flat @click.native="cancelprj = true">Cancel</v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="2">
+        <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px" />
+        <v-btn primary @click.native="prj = 3">Continue</v-btn>
+        <v-btn flat @click.native="prj = 1">Back</v-btn>
+        <v-btn flat @click.native="cancelprj = true">Cancel</v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="3">
+        <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px" />
+        <v-btn primary @click.native="">Confirm and Pay</v-btn>
+        <v-btn flat @click.native="prj = 2">Back</v-btn>
+        <v-btn flat @click.native="cancelprj = true">Cancel</v-btn>
+      </v-stepper-content>
+    </v-stepper>
+
   </div>
 </template>
 
@@ -14,6 +50,12 @@ export default {
   components: {
     Calendar,
     Session
+  },
+  data () {
+    return {
+      cancelprj: false,
+      prj: 0
+    }
   }
 }
 </script>
