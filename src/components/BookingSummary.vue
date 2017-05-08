5cc113bd7eec6f684c7d798c515377d45efa4f36
Krishna Kumar <kks32@cam.ac.uk>
2017-05-08 15:20:33 +0100

    :house: Address

--- src/components/BookingSummary.vue
+++ src/components/BookingSummary.vue
@@ -11,31 +11,82 @@
       <v-container fluid>
         <v-row>
           <v-col xs6>
-            <h6>Booking reference</h6>
+            <v-subheader v-text="'Booking reference'"/>
           </v-col>
           <v-col xs6>
-            <h6>{{this.$store.getters['purchase/getuuid']}}</h6>
+            <v-subheader>{{this.$store.getters['purchase/getuuid']}}</v-subheader>
           </v-col>
         </v-row>
         <v-row>
           <v-col xs6>
-            <h6>Booking date and sessions:</h6>
+            <v-subheader v-text="'Booking date and sessions'"/>
           </v-col>
           <v-col xs6>
-            <h6>{{this.$store.getters['purchase/getdate']}} {{this.$store.getters['purchase/getsession']}}</h6>
+            <v-subheader>{{this.$store.getters['purchase/getdate']}} {{this.$store.getters['purchase/getsession']}}</v-subheader>
           </v-col>
         </v-row>
         <v-row>
           <v-col xs6>
-            <h6>Total for {{this.$store.getters['purchase/getntickets']}} tickets and  {{this.$store.getters['purchase/get_nguidebooks']}} guides is:</h6>
+            <v-subheader>Total for {{this.$store.getters['purchase/getntickets']}} tickets and  {{this.$store.getters['purchase/get_nguidebooks']}} guides is:</v-subheader>
           </v-col>
           <v-col xs6>
-            <h6> £{{this.$store.getters['purchase/gettotal']}}</h6>
+            <v-subheader> £{{this.$store.getters['purchase/gettotal']}}</v-subheader>
           </v-col>
         </v-row>
+
       </v-container>
     </v-card-text>
   </v-card>
+  <v-card-row class="purple darken-4">
+    <v-card-title>
+      <span class="white--text">Personal details</span>
+      <v-spacer></v-spacer>
+    </v-card-title>
+  </v-card-row>
+  <v-card-text class="grey lighten-4 elevation-1">
+    <v-container fluid>
+      <v-row>
+        <v-col xs6>
+          <v-subheader v-text="'Full name'" />
+        </v-col>
+        <v-col xs6>
+          <v-text-field
+            label="Full name"
+            counter
+            v-model="name"
+            min="5"
+            max="255"
+            required
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col xs6>
+          <v-subheader v-text="'Email address to subscribe to mailing list'" />
+        </v-col>
+        <v-col xs6>
+          <v-text-field
+            label="someone@anonymous.com"
+            counter
+            v-model="email"
+            min="5"
+            max="255"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col xs6>
+          <v-subheader v-text="'Gift aid'" />
+        </v-col>
+        <v-col xs6>
+          <v-radio label="Yes, I pay taxes in the UK. Gift aid this purchase" primary v-model="giftaid" value="true" light />
+          <v-radio label="No" primary v-model="giftaid" value="false" light />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card-text>
+</v-card>
+
 </div>
 </template>
 
@@ -44,7 +95,9 @@ export default {
   'name': 'Summary',
   data () {
     return {
-      msg: 'Hi'
+      name: '',
+      email: '',
+      giftaid: 'true'
     }
   }
 }
