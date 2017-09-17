1083abe43610b213957aaa29f9d29b05d2077a3f
Krishna Kumar <kks32@cam.ac.uk>
2017-09-17 16:38:47 +0100

    :hammer: :construction: Fix header and footer layout #12

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="purple darken-4">
+  <v-footer class="purple darken-4 white--text">
     <div class="text-xs-right">© 2017 King's College, Cambridge</div>
   </v-footer>
 </template>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,7 +1,9 @@
 <template>
-  <v-toolbar class="purple darken-4">
-  <v-toolbar-title class="hidden-sm-and-down">{{title}}</v-toolbar-title>
-  <v-toolbar-items v-for="(item,i) in items" :key="i">
+  <v-toolbar class="purple darken-4 white--text">
+  <v-toolbar-title>{{title}}</v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+  <v-toolbar-items class="hidden-sm-and-down" v-for="(item,i) in items" :key="i">
     <v-toolbar-item :href="item.link" :router="item.router">{{item.title}}</v-toolbar-item>
   </v-toolbar-items>
   </v-toolbar>
