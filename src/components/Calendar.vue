53e63ca7e22e67467eca4f569f051978f771deaf
Krishna Kumar <kks32@cam.ac.uk>
2017-09-17 21:06:29 +0100

    :construction: Vuetify calendar #11

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,78 +1,25 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col xs12>
-        <v-subheader>Select date: {{startTime.time}}</v-subheader>
-        <v-card class="lighten-4 elevation-0">
-          <v-card-text>
-            <date-picker :date="startTime" :option="option" :limit="limit"></date-picker>
-            <v-btn v-if="startTime.time!=''" class="primary white--text" @click.native="setdate()">Accept date<v-icon right>check_circle</v-icon></v-btn>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-layout row wrap>
+      <v-flex xs12 class="hidden-xs-only">
+        <v-date-picker class="mt-3" v-model="date" :allowed-dates="allowedDates" landscape></v-date-picker>
+         <v-btn v-if="this.date!=''" class="primary white--text" @click.native="setdate()">Accept: {{date}}<v-icon right>check_circle</v-icon></v-btn>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-import Calendar from './vue-datepicker-es6'
 
 export default {
   data () {
     return {
-      startTime: {
-        time: ''
-      },
-      option: {
-        type: 'day',
-        week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-        month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        format: 'YYYY-MM-DD',
-        placeholder: 'when?',
-        inputStyle: {
-          'display': 'inline-block',
-          'padding': '6px',
-          'line-height': '22px',
-          'font-size': '16px',
-          'border': '2px solid #fff',
-          'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 0.2)',
-          'border-radius': '2px',
-          'color': '#5F5F5F'
-        },
-        color: {
-          header: '#0000FF',
-          headerText: '#FFFFFF'
-        },
-        buttons: {
-          ok: 'Ok',
-          cancel: 'Cancel'
-        },
-        overlayOpacity: 0.5, // 0.5 as default
-        dismissible: false // as true as default
-      },
-      timeoption: {
-        type: 'min',
-        week: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-        month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        format: 'YYYY-MM-DD HH:mm'
-      },
-      limit: [{
-        type: 'weekday',
-        available: [1, 2, 3, 4, 5, 6]
-      },
-      {
-        type: 'fromto',
-        from: '2017-07-30',
-        to: '2017-09-30'
-      }]
+      date: ''
     }
-  },
-  components: {
-    'date-picker': Calendar
   },
   methods: {
     setdate () {
-      this.$store.dispatch('purchase/setdate', this.startTime.time)
+      this.$store.dispatch('purchase/setdate', this.date)
     }
   }
 }
