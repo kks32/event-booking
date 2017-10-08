44f7ad38ff90cd6bbcea67c3c2213977b8ef4107
Krishna Kumar <kks32@cam.ac.uk>
2017-10-08 15:33:37 -0700

    :hammer: Fixes #18 get bookings on a date

--- src/components/Bookings.vue
+++ src/components/Bookings.vue
@@ -2,12 +2,19 @@
   <v-layout row wrap>
     <v-flex>
       <h4 class="mb-3">Bookings</h4>
+      <v-date-picker
+        class="mt-3"
+        v-model="date"
+        first-day-of-week=1
+        :allowed-dates="allowedDates"
+        color="primary"
+        landscape></v-date-picker>
       <v-card>
         <v-list three-line>
           <template v-for="session in sessions">
             <v-subheader class="bold grey lighten-1">{{session}} bookings</v-subheader>
             <template v-for="booking in bookings" v-if="booking.session === session">
-              <v-list-tile avatar v-bind:key="booking.name" :href="baseurl + date">
+              <v-list-tile avatar v-bind:key="booking.name">
                 <v-avatar>
                   <img src="/static/media/kings.png" style="width:50px;height:50px;" alt="kings">
                 </v-avatar>
@@ -32,21 +39,44 @@ export default {
   name: 'bookings',
   data () {
     return {
-      date: '2017-10-19',
+      date: null,
+      allowedDates: [],
       baseurl: '/bookings/',
       sessions: ['Morning', 'Aternoon'],
       bookings: []
     }
   },
   created () {
-    HTTP.get('bookings/date/' + this.date)
-      .then(response => {
-        // JSON responses are automatically parsed.
-        this.bookings = response.data
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+    HTTP.get(`dates`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.allowedDates = response.data
+      if (this.allowedDates.length > 0) {
+        this.date = this.allowedDates[0]
+      } else {
+        this.date = null
+      }
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  },
+  methods: {
+    fetchbookings () {
+      HTTP.get('bookings/date/' + this.date)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.bookings = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
+  },
+  watch: {
+    date () {
+      this.fetchbookings()
+    }
   }
 }
 </script>
