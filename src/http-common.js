8e05a8277800ef913ad083303a572710f0886cfa
Krishna Kumar <kks32@cam.ac.uk>
2017-12-11 22:58:55 +0000

    :lock: HTTPS connection to API

--- src/http-common.js
+++ src/http-common.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 
 // Define a baseURL
 export const HTTP = axios.create({
-  baseURL: `http://api.kingscollegecam.com:4000/`
+  baseURL: `https://api.kingscollegecam.com:4000/`
 })
