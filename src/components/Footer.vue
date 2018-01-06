84ade8d217996deb232859050e5f7cff447e7a15
Krishna Kumar <kks32@cam.ac.uk>
2018-01-06 15:23:05 +0000

    :art: Modify footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,14 @@
 <template>
   <v-footer class="purple darken-4 white--text text-xs-center">
-    <p class="text-xs-right">
-      © 2017 King's College, Cambridge
-    </p>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <img src="static/media/payment-cards-footer.png" style="width:30%" alt="Payment cards">
+      </v-flex>
+      <v-flex xs6>
+        <p>
+          © 2017 King's College, Cambridge
+        </p>
+      </v-flex>
+    </v-layout>
   </v-footer>
 </template>
